d9a4bad9717f3036a92b7f966ac0e5248655112f
Mark Harrison <marhar@gmail.com>
2024-07-18 18:27:31 -0700

    fully publishing

--- sync-to-io.sh
+++ sync-to-io.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+cd /Users/marhar/g/website
+hugo
 rm -rf /Users/marhar/g/marhar.github.io/*
 cp -rp /Users/marhar/g/website/public/* /Users/marhar/g/marhar.github.io
+
+cd /Users/marhar/g/marhar.github.io
+git commit -am "$1"
+git push
